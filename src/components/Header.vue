752d8d89ad4f8619257a2c56d62dd2c8ff58b207
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-04-12 15:51:14 +0300

    Add files via upload

---
+++ src/components/Header.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="header">
+    <div class="cont1">
+      <img src="../assets/images/logo.png" alt="">
+    </div>
+    <div class="cont2">
+      <div class="search">
+        <div class="search_input">
+          <img src="../assets/images/search.png" alt="">
+          <input type="text" placeholder="Search" class="search_form">
+        </div>
+        <button class="notification">
+          <img src="../assets/images/notifications.png" alt="">
+          <span class="notification_num">47</span>
+        </button>
+      </div>
+      <div class="user">
+        <img src="../assets/images/user.png" alt="">
+        <div>
+          <p>Джонатан Доу</p>
+          <span>CEO</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Header"
+}
+</script>
+
+<style scoped lang="scss">
+.header{
+  background-color: #437FD7;
+  height: 80px;
+  display: flex;
+  .cont1{
+    width: 275px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .cont2{
+    display: flex;
+    width: calc(100vw - 275px);
+    justify-content: space-between;
+    .search{
+      display: flex;
+      align-items: center;
+      .search_input{
+        margin: 0 37px 0 20px;
+        height: 30px;
+        width: 258px;
+        >img {
+          position: absolute;
+          padding: 7.07px 14.5px;
+        }
+        >input{
+          background: #FFFFFF;
+          /* Shadow */
+
+          box-shadow: 0px 4px 4px rgba(51, 51, 51, 0.04), 0px 4px 16px rgba(51, 51, 51, 0.08);
+          border-radius: 4px;
+          border: none;
+          outline: none;
+          padding: 4px 44px;
+        }
+      }
+      .notification{
+        border: none;
+        background: transparent;
+        outline: none;
+        cursor: pointer;
+        >span {
+          width: 35px;
+          height: 13px;
+          padding-top: 4px;
+
+          //padding: 4px 12px 3px 12px;
+          position: absolute;
+          color: #E80B0B;
+          font-weight: bold;
+          font-size: 8px;
+          background: #FFFFFF;
+          border-radius: 5px 5px 5px 0px;
+        }
+      }
+    }
+    .user{
+      width: 210px;
+      margin-right: 60px;
+      display: flex;
+      align-items: center;
+      >div{
+        margin-left: 20px;
+        color: #FFFFFF;
+        >p{
+          font-size: 20px;
+          line-height: 24px;
+          margin: 0;
+        }
+        >span{
+          font-size: 16px;
+          line-height: 19px;
+          font-weight: bold;
+        }
+      }
+      >img{
+        width: 40px;
+        height: 40px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="sidebar">
+    <div class="item" v-for="(item,index) in items" :key="index">
+      <div @click="isSelect(index)">
+        <router-link class="li" :class="{'li-active': item.isSelected}" :to="item.to">
+          <img :src="item.icon" alt="">
+          <span>{{item.title}}</span>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+  data(){
+    return {
+      items: [
+        {
+          title: "Профиль",
+          to: "/profile",
+          icon: require("../assets/images/profile.png"),
+          isSelected: true,
+        },
+        {
+          title: "Главная",
+          to: "/",
+          icon: require("../assets/images/main.png"),
+          isSelected: false,
+        },
+        {
+          title: "Персонал",
+          to: "/personal",
+          icon: require("../assets/images/personal.png"),
+          isSelected: false,
+        },
+        {
+          title: "Статистика",
+          to: "/statistics",
+          icon: require("../assets/images/statistics.png"),
+          isSelected: false,
+        },
+        {
+          title: "Журнал",
+          to: "/journal",
+          icon: require("../assets/images/journal.png"),
+          isSelected: false,
+        },
+        {
+          title: "Клиенты",
+          to: "/clients",
+          icon: require("../assets/images/clients.png"),
+          isSelected: false,
+        },
+        {
+          title: "Услуги",
+          to: "/services",
+          icon: require("../assets/images/services.png"),
+          isSelected: false,
+        },
+        {
+          title: "Финансы",
+          to: "/finance",
+          icon: require("../assets/images/finance.png"),
+          isSelected: false,
+        },
+        {
+          title: "Ивенты",
+          to: "/events",
+          icon: require("../assets/images/event.png"),
+          isSelected: false,
+        },
+        {
+          title: "Настройки",
+          to: "/settings",
+          icon: require("../assets/images/settings.png"),
+          isSelected: false,
+        },
+        {
+          title: "Выйти",
+          to: "/login",
+          icon: require("../assets/images/logout.png"),
+
+        },
+      ]
+    }
+  },
+  methods: {
+    isSelect(i){
+      for(let i=0; i<this.items.length; i++){
+        this.items[i].isSelected = false;
+      }
+      this.items[i].isSelected = true;
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.sidebar{
+  width: 275px;
+  display: flex;
+  flex-flow: column;
+  height: 100vh;
+  padding-top: 70px;
+    .item {
+      font-size: 20px;
+      text-align: center;
+      >div{
+        .li{
+          display: flex;
+          width: 270px;
+          align-items: center;
+          float: right;
+          height: 40px;
+          >img{
+            width: 16.67px;
+            height: 16.67px;
+            padding: 0 22px 0 27px;
+          }
+          >span{
+            color: #437FD7 !important;
+            text-decoration: none !important;
+          }
+        }
+        .li:hover{
+          background: rgba(67, 127, 215, 0.2);
+          width: 275px;
+        }
+        .li-active{
+          border-left: 5px solid #437FD7 !important;
+          background: rgba(67, 127, 215, 0.2);
+        }
+      }
+    }
+}
+</style>
