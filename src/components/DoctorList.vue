974b4a76d737425b295de9647836f938b8a4ed20
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-04-15 19:37:39 +0300

    Add files via upload

---
+++ src/components/DoctorList.vue
@@ -0,0 +1,196 @@
+<template>
+  <div class="doctor_list">
+    <div class="list">
+      <div class="vfor" v-for="(c,i) in cards">
+        <div class="card">
+          <img src="../assets/images/Userprofile.png" alt="">
+          <div class="userinfo">
+            <p class="username">Каиргельды Жолдасбеков</p>
+
+            <div class="df">
+              <button class="btn">Гинеколог</button>
+              <button class="btn">Ординатор</button>
+            </div>
+          </div>
+          <img src="../assets/images/details.svg" class="details" alt="">
+        </div>
+      </div>
+      <div class="list_num">
+        <button class="list_btn prev">Назад</button>
+
+        <button class="num_of_list">1</button>
+        <button class="num_of_list disable">2</button>
+        <button class="num_of_list disable">3</button>
+
+        <button class="list_btn next">Вперед</button>
+
+      </div>
+
+
+
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DoctorList",
+  data() {
+    return{
+      cards: [
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+        {
+          username: "Каиргельды Жолдасбеков",
+          icon: require("../assets/images/Userprofile.png")
+        },
+
+      ]
+    }
+  }
+
+}
+</script>
+
+<style scoped lang="sass">
+  .doctor_list
+    height: 100%
+    margin-left: 20px
+    .list
+      display:  flex
+      width: 64.239583333333336VW
+      flex-wrap: wrap
+      .vfor
+        .card
+          width: 17.322917VW
+          position: relative
+          height: 6.508333333333334VW
+          background: #FFFFFF
+          box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.1)
+          border-radius: 10px
+          display: flex
+          padding: 1.0416666666666665VW
+          margin-bottom: 1.0416666666666665VW
+          margin-right: 1.0416666666666665VW
+          .details
+            position: absolute
+            right: 20px
+            top: 0px
+            width: 4.33px
+          img
+            width: 3.6458333333333335VW
+            height: 3.6458333333333335VW
+          .userinfo
+            margin-left: 1.5625VW
+            .username
+              font-weight: 600
+              font-size: 1.25VW
+              line-height: 29px
+              display: flex
+              align-items: center
+              color: #437FD7
+              margin: 0
+            .df
+              .btn
+                width: 4.635416666666667VW
+                height: 1.40625VW
+                margin-top: 1.1979166666666667VW
+                color: #FFFFFF
+                border: none
+                outline: none
+                cursor: pointer
+                background: #EE5396
+                border-radius: 5px
+                font-size: 0.7291666666666666VW
+              .btn:first-child
+                background: #007D79
+                margin-right: 10px
+      .list_num
+        display: flex
+        align-items: center
+        justify-content: center
+        width: 100%
+         margin: 0 auto
+        margin-top: 40px
+        .list_btn
+          width: 61px
+          height: 30px
+          border-radius: 5px
+          padding: 5px 10px
+          border: none
+          outline: none
+          cursor: pointer
+          font-weight: 600
+          font-size: 0.7291666666666666VW
+          line-height: 17px
+        .prev
+          color: #333333
+          background: #D9E8FF
+          margin-right: 30px
+        .num_of_list
+          color: #437FD7
+          font-weight: 600
+          font-size: 0.7291666666666666VW
+          line-height: 17px
+          border: 1px solid #437FD7
+          box-sizing: border-box
+          background: transparent
+          border-radius: 5px
+          width: 30px
+          height: 30px
+          outline: none
+          cursor: pointer
+          margin-right: 20px
+        .num_of_list.disable
+          border: 1px solid #D9E8FF
+          color: #D9E8FF
+        .next
+          margin-left: 10px
+          background: #437FD7
+          color: #FFFFFF
+
+</style>
---
+++ src/components/Personal.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="container">
+    <h1 class="pagename">Персонал</h1>
+    <div class="subheader">
+      <p class="road">
+        <span class="from">MedApp</span>
+        <img src="../assets/images/arrow-right.svg" alt="">
+        <span class="to">Персонал</span>
+      </p>
+
+      <button class="create">
+        <img src="../assets/images/create.svg" alt="">
+        Добавить
+      </button>
+    </div>
+
+    <div class="sub_container">
+      <FilterDoctors />
+      <DoctorList />
+
+    </div>
+  </div>
+</template>
+
+<script>
+import FilterDoctors from "@/components/FilterDoctors";
+import DoctorList from "@/components/DoctorList";
+export default {
+name: "Personal",
+  components: {DoctorList, FilterDoctors}
+}
+</script>
+
+<style scoped lang="sass">
+
+.container
+  padding-bottom: 53px
+  width: 100%
+  margin-left: 20px
+  .pagename
+    font-family: Proxima Nova, sans-serif
+    font-style: normal
+    font-weight: 600
+    font-size: 24px
+    line-height: 29px
+    display: flex
+    align-items: center
+    color: #437FD7
+  .subheader
+    display: flex
+    justify-content: space-between
+    padding-right: 4.458333333333335VW
+    .road
+      font-style: normal
+      font-weight: 600
+      font-size: 16px
+      line-height: 19px
+      display: flex
+      align-items: center
+      color: #A6C8FF
+      img
+        margin-left: 16px
+        margin-right: 16px
+      .to
+        color: #437FD7
+        font-style: normal
+        font-weight: 600
+        font-size: 16px
+        line-height: 19px
+    .create
+      display: flex
+      justify-content: center
+      cursor: pointer
+      outline: none
+      align-items: center
+      padding: 0
+      background-color: transparent
+      border: none
+      font-style: normal
+      font-weight: 600
+      font-size: 14px
+      line-height: 17px
+      color: #437FD7
+      img
+        margin-right: 10.05px
+
+  .sub_container
+    padding-top: 30px
+    display: flex
+
+</style>
