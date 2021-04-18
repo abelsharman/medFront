2a9dec9bbd28ab81a479419a24ed7ed89048a70c
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-04-18 06:01:28 +0600

    Update SideBar.vue

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,10 +2,10 @@
   <div class="sidebar">
     <div class="item" v-for="(item,index) in items" :key="index">
       <div @click="isSelect(index)">
-        <router-link class="li" :class="{'li-active': item.isSelected}" :to="item.to">
+        <div class="li" :class="{'li-active': item.isSelected}" @click="selectNav(item.to)" >
           <img :src="item.icon" alt="">
           <span>{{item.title}}</span>
-        </router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -19,67 +19,67 @@ export default {
       items: [
         {
           title: "Профиль",
-          to: "/profile",
+          to: "0",
           icon: require("../assets/images/profile.png"),
           isSelected: true,
         },
         {
           title: "Главная",
-          to: "/",
+          to: "1",
           icon: require("../assets/images/main.png"),
           isSelected: false,
         },
         {
           title: "Персонал",
-          to: "/personal",
+          to: "2",
           icon: require("../assets/images/personal.png"),
           isSelected: false,
         },
         {
           title: "Статистика",
-          to: "/statistics",
+          to: "3",
           icon: require("../assets/images/statistics.png"),
           isSelected: false,
         },
         {
           title: "Журнал",
-          to: "/journal",
+          to: "4",
           icon: require("../assets/images/journal.png"),
           isSelected: false,
         },
         {
           title: "Клиенты",
-          to: "/clients",
+          to: "5",
           icon: require("../assets/images/clients.png"),
           isSelected: false,
         },
         {
           title: "Услуги",
-          to: "/services",
+          to: "6",
           icon: require("../assets/images/services.png"),
           isSelected: false,
         },
         {
           title: "Финансы",
-          to: "/finance",
+          to: "7",
           icon: require("../assets/images/finance.png"),
           isSelected: false,
         },
         {
           title: "Ивенты",
-          to: "/events",
+          to: "8",
           icon: require("../assets/images/event.png"),
           isSelected: false,
         },
         {
           title: "Настройки",
-          to: "/settings",
+          to: "9",
           icon: require("../assets/images/settings.png"),
           isSelected: false,
         },
         {
           title: "Выйти",
-          to: "/login",
+          to: "/logout",
           icon: require("../assets/images/logout.png"),
 
         },
@@ -92,6 +92,9 @@ export default {
         this.items[i].isSelected = false;
       }
       this.items[i].isSelected = true;
+    },
+    selectNav(i){
+      this.$store.state.navItem = i
     }
   }
 }
@@ -105,40 +108,41 @@ export default {
   flex-flow: column;
   height: 100vh;
   padding-top: 3.6458333333333335VW;
-    .item {
-      font-family: Proxima Nova, sans-serif;
-      font-style: normal;
-      font-weight: normal;
-      font-size: 1.0416666666666665VW;
-      line-height: 24px;
-      color: #437FD7;
-      >div{
-        .li{
-          display: flex;
-          width: 14.0625VW;
-          align-items: center;
-          float: right;
-          height: 40px;
-          >img{
-            width: 16.67px;
-            height: 16.67px;
-            padding: 0 22px 0 27px;
-          }
-          >span{
-            color: #437FD7 !important;
-            text-decoration: none !important;
-          }
+  .item {
+    font-family: Proxima Nova, sans-serif;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 1.0416666666666665VW;
+    line-height: 24px;
+    color: #437FD7;
+    >div{
+      .li{
+        display: flex;
+        width: 14.0625VW;
+        align-items: center;
+        float: right;
+        height: 40px;
+        cursor: pointer;
+        >img{
+          width: 16.67px;
+          height: 16.67px;
+          padding: 0 22px 0 27px;
         }
-        .li:hover{
-          background: rgba(67, 127, 215, 0.2);
-          width: 14.0625VW;
-          border-left: 5px solid rgba(67, 127, 215, 0.0);
-        }
-        .li-active{
-          border-left: 5px solid #437FD7 !important;
-          background: rgba(67, 127, 215, 0.2);
+        >span{
+          color: #437FD7 !important;
+          text-decoration: none !important;
         }
       }
+      .li:hover{
+        background: rgba(67, 127, 215, 0.2);
+        width: 14.0625VW;
+        border-left: 5px solid rgba(67, 127, 215, 0.0);
+      }
+      .li-active{
+        border-left: 5px solid #437FD7 !important;
+        background: rgba(67, 127, 215, 0.2);
+      }
     }
+  }
 }
 </style>
