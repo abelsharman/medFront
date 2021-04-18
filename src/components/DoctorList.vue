0d7fb2897061e04d199204e7f0d90febd005420b
Yer12 <55184621+Yer12@users.noreply.github.com>
2021-04-18 06:00:17 +0600

    Update DoctorList.vue

--- src/components/DoctorList.vue
+++ src/components/DoctorList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="doctor_list">
     <div class="list">
-      <div class="vfor" v-for="(c,i) in cards">
+      <div class="vfor" v-for="(c,i) in cards" :key="i">
         <div class="card">
           <img src="../assets/images/Userprofile.png" alt="">
           <div class="userinfo">
@@ -97,100 +97,100 @@ export default {
 </script>
 
 <style scoped lang="sass">
-  .doctor_list
-    height: 100%
-    margin-left: 20px
-    .list
-      display:  flex
-      width: 64.239583333333336VW
-      flex-wrap: wrap
-      .vfor
-        .card
-          width: 18.052083333333336VW
-          position: relative
-          height: 6.508333333333334VW
-          background: #FFFFFF
-          box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.1)
-          border-radius: 10px
-          display: flex
-          padding: 1.0416666666666665VW
-          margin-bottom: 1.0416666666666665VW
-          margin-right: 1.0416666666666665VW
-          .details
-            position: absolute
-            right: 20px
-            top: 0px
-            width: 4.33px
-          img
-            width: 3.6458333333333335VW
-            height: 3.6458333333333335VW
-          .userinfo
-            margin-left: 1.5625VW
-            .username
-              font-weight: 600
-              font-size: 1.25VW
-              line-height: 29px
-              display: flex
-              align-items: center
-              color: #437FD7
-              margin: 0
-            .df
-              .btn
-                width: 4.635416666666667VW
-                height: 1.40625VW
-                margin-top: 1.1979166666666667VW
-                color: #FFFFFF
-                border: none
-                outline: none
-                cursor: pointer
-                background: #EE5396
-                border-radius: 5px
-                font-size: 0.7291666666666666VW
-              .btn:first-child
-                background: #007D79
-                margin-right: 10px
-      .list_num
+.doctor_list
+  height: 100%
+  margin-left: 20px
+  .list
+    display:  flex
+    width: 64.239583333333336VW
+    flex-wrap: wrap
+    .vfor
+      .card
+        width: 18.052083333333336VW
+        position: relative
+        height: 6.508333333333334VW
+        background: #FFFFFF
+        box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.1)
+        border-radius: 10px
         display: flex
-        align-items: center
-        justify-content: center
-        width: 100%
-         margin: 0 auto
-        margin-top: 40px
-        .list_btn
-          width: 61px
-          height: 30px
-          border-radius: 5px
-          padding: 5px 10px
-          border: none
-          outline: none
-          cursor: pointer
-          font-weight: 600
-          font-size: 0.7291666666666666VW
-          line-height: 17px
-        .prev
-          color: #333333
-          background: #D9E8FF
-          margin-right: 30px
-        .num_of_list
-          color: #437FD7
-          font-weight: 600
-          font-size: 0.7291666666666666VW
-          line-height: 17px
-          border: 1px solid #437FD7
-          box-sizing: border-box
-          background: transparent
-          border-radius: 5px
-          width: 30px
-          height: 30px
-          outline: none
-          cursor: pointer
-          margin-right: 20px
-        .num_of_list.disable
-          border: 1px solid #D9E8FF
-          color: #D9E8FF
-        .next
-          margin-left: 10px
-          background: #437FD7
-          color: #FFFFFF
+        padding: 1.0416666666666665VW
+        margin-bottom: 1.0416666666666665VW
+        margin-right: 1.0416666666666665VW
+        .details
+          position: absolute
+          right: 20px
+          top: 0px
+          width: 4.33px
+        img
+          width: 3.6458333333333335VW
+          height: 3.6458333333333335VW
+        .userinfo
+          margin-left: 1.5625VW
+          .username
+            font-weight: 600
+            font-size: 1.25VW
+            line-height: 29px
+            display: flex
+            align-items: center
+            color: #437FD7
+            margin: 0
+          .df
+            .btn
+              width: 4.635416666666667VW
+              height: 1.40625VW
+              margin-top: 1.1979166666666667VW
+              color: #FFFFFF
+              border: none
+              outline: none
+              cursor: pointer
+              background: #EE5396
+              border-radius: 5px
+              font-size: 0.7291666666666666VW
+            .btn:first-child
+              background: #007D79
+              margin-right: 10px
+    .list_num
+      display: flex
+      align-items: center
+      justify-content: center
+      width: 100%
+        margin: 0 auto
+      margin-top: 40px
+      .list_btn
+        width: 61px
+        height: 30px
+        border-radius: 5px
+        padding: 5px 10px
+        border: none
+        outline: none
+        cursor: pointer
+        font-weight: 600
+        font-size: 0.7291666666666666VW
+        line-height: 17px
+      .prev
+        color: #333333
+        background: #D9E8FF
+        margin-right: 30px
+      .num_of_list
+        color: #437FD7
+        font-weight: 600
+        font-size: 0.7291666666666666VW
+        line-height: 17px
+        border: 1px solid #437FD7
+        box-sizing: border-box
+        background: transparent
+        border-radius: 5px
+        width: 30px
+        height: 30px
+        outline: none
+        cursor: pointer
+        margin-right: 20px
+      .num_of_list.disable
+        border: 1px solid #D9E8FF
+        color: #D9E8FF
+      .next
+        margin-left: 10px
+        background: #437FD7
+        color: #FFFFFF
 
 </style>
