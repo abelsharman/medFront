2fb0181c1ebd301769685828e91406fcd18a6fa4
abelsharman <abelsharman85@gmail.com>
2021-05-23 17:20:45 +0600

    new:graph stat

--- src/components/Primary.vue
+++ src/components/Primary.vue
@@ -20,7 +20,25 @@
 
             <div class="primary_statistics_2">
                 <div class="primary_percentage">
-                    <p>3</p>
+                    <p>Процентаж оказываемых услуг</p>
+                    <div class="primary_percentage_block" v-if="percent == 1">
+                        <apexchart width="90%" style="text-align: left;" type="donut" :options="chartOptions" :series="series"></apexchart>
+                    </div>
+
+                    <div class="primary_percentage_block" v-if="percent == 2">
+                        <apexchart width="90%" style="text-align: left;" type="donut" :options="chartOptions" :series="series"></apexchart>
+                    </div>
+
+                    <div class="primary_percentage_block" v-if="percent == 3">
+                        <apexchart width="90%" style="text-align: left;" type="donut" :options="chartOptions" :series="series"></apexchart>
+                    </div>
+
+
+                    <div class="primary_percentage_nav">
+                        <h3 v-bind:class="{primary_percentage_nav_clicked : percent == 1}" @click="weakInfo">За неделю</h3>
+                        <h3 v-bind:class="{primary_percentage_nav_clicked : percent == 2}" @click="monInfo">За месяц</h3>
+                        <h3 v-bind:class="{primary_percentage_nav_clicked : percent == 3}" @click="yearInfo">За год</h3>
+                    </div>
                 </div>
 
                 <div class="primary_graph">
@@ -34,7 +52,9 @@
             
 
             <div class="primary_chat_block">
-                <p>чат</p>
+                <div id="chart">
+                    <p>чат</p>
+                </div>
             </div>
         </div>
     </div>
@@ -42,7 +62,62 @@
 
 <script>
 export default {
-    name: 'Primary'
+    name: 'Primary',
+    data(){
+        return{   
+            percent: 1,   
+            series: [44, 55, 13, 33, 22, 66, 20],
+            widthScreen: window.innerWidth,
+            chartOptions: {
+                chart: {
+                  type: 'donut',
+                },
+                labels: ['Гинекология', 'Кардиология', 'Стоматология', 'Хирургия', 'Травмотология', 'Онкология', 'Ревматология'],
+                dataLabels: {
+                  enabled: false
+                },
+                responsive: [{
+                  options: {
+                    chart: {
+                    },
+                    legend: {
+                      show: false
+                    }
+                  }
+                }],
+                legend: {
+                    position: 'right',
+                    offsetY: 50,
+                    offsetX: 0,
+                    fontFamily: 'Proxima Nova, sans-serif',
+                    fontWeight: '600',
+                    width: 125,
+                    height: 200,
+                    itemMargin: {
+                        horizontal: 5,
+                        vertical: 4
+                    },
+
+                },
+            }
+        } 
+    },
+    methods:{
+        weakInfo(){
+            this.percent = 1
+            this.series = [44, 55, 13, 33, 22, 66, 20]
+        },
+        monInfo(){
+            this.percent = 2
+            this.series = [40, 54, 16, 13, 42, 26, 50]
+        },
+        yearInfo(){
+            this.percent = 3
+            this.series = [30, 64, 20, 10, 32, 56, 70]
+        },
+    },
+    mounted(){
+    },
 }
 </script>
 
@@ -60,6 +135,7 @@ export default {
     .primary
         animation: appearingBlock 0.5s 
         margin: 0 0 0 20px
+        font-family: Proxima Nova, sans-serif
         height: 80vh
         width: 85vw
         margin-left: 15.7vw
@@ -105,18 +181,67 @@ export default {
                 display: inline-block
                 width: 27vw
 
-                div
+                
+
+                .primary_percentage
+                    position: relative
+                    height: 37vh
                     background: #FFFFFF
                     box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.1)
                     border-radius: 10px
                     width: 100%
                     margin-bottom: 1vh
                     text-align: center
+                    .primary_percentage_block
+                        animation: appearingBlock 0.5s
+                    .primary_percentage_nav
+                        background: #D9E8FF
+                        border-radius: 5px
+                        width: 60%
+                        height: 30px
+                        margin-top: 20px
+                        margin-left: 20%
+                        
+                        h3
+                            display: inline-block
+                            width: 33%
+                            background: #D9E8FF
+                            color: black
+                            font-weight: 600
+                            font-size: 0.625vw //12px
+                            line-height: 0.78125vw //15px
+                            margin: 0
+                            height: 24px
+                            padding-top: 6px
+                            vertical-align: top
+                            cursor: pointer
+                            transition: 0.5s all ease
+                        .primary_percentage_nav_clicked
+                            background: #437FD7
+                            color: white
+                            border-radius: 5px
 
-                .primary_percentage
-                    height: 37vh
+                    p
+                        font-family: Proxima Nova, sans-serif
+                        position: absolute
+                        left: 60%
+                        width: 35%
+                        text-align: left
+                        top: 15px
+                        font-style: normal
+                        font-weight: 600
+                        font-size: 0.833333333vw //16px
+                        line-height: 0.98958333333333vw //19px
+                        color: #437FD7
+
                 .primary_graph
                     height: 37vh
+                    background: #FFFFFF
+                    box-shadow: 0px 4px 25px rgba(0, 0, 0, 0.1)
+                    border-radius: 10px
+                    width: 100%
+                    margin-bottom: 1vh
+                    text-align: center
 
 
 
--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueApexCharts from 'vue-apexcharts'
+
+
+Vue.component('apexchart', VueApexCharts)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  VueApexCharts,
   render: h => h(App)
 }).$mount('#app')
